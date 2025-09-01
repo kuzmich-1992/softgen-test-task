ac71e850d48f7daf7a6e6dcd1a89509524866a28
Dima <kuzmichman92@gmail.com>
2025-09-01 22:55:18 +0300

    add_user_appointment_relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,26 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :appointments, class_name: "Appointment",
+                          foreign_key: "creator_id",
+                          dependent: :destroy
+  has_many :recipients, through: :appointments, source: :recipient
+
+  has_many :appointments, class_name: "Appointment",
+                          foreign_key: "recipient_id",
+                          dependent: :destroy
+  has_many :creators, through: :appointments, source: :creator
+
+  def create_appointment(other_user)
+    active_relationships.create(recipient_id: other_user.id)
+  end
+
+  def delete_appointment(other_user)
+    active_relationships.find_by(recipient_id: other_user.id).destroy
+  end
+
+  def appointment_present?(other_user)
+    recipients.include?(other_user)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -32,5 +32,4 @@ ActiveRecord::Schema[7.0].define(version: 2025_09_01_174303) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
