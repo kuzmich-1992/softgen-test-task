4d4a4158cef87b161c282477015506772893c989
Dima <kuzmichman92@gmail.com>
2025-09-03 00:07:45 +0300

    categories_dropdown_and_logic_added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#index'
   get 'pages/index'
+  get 'pages/filter_categories', to: 'pages#filter_categories', as: 'filter_categories'
   post 'appointments/create', to: 'appointments#create', as: 'create_appointment'
   patch 'appointments/create', to: 'appointments#update', as: 'update_appointment'
 end
