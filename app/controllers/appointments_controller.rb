ed2f165d289bc0865bf4b72976ecdf3be60fa20f
Dima <kuzmichman92@gmail.com>
2025-09-02 20:51:59 +0300

    maximum_10_open_appointments_added

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,16 +1,16 @@
 class AppointmentsController < ApplicationController
   def create
     @user = User.find(params[:user_id])
-    @user.create_appointment(current_user, @user)
-    redirect_to root_path
+    if @user.appointments.where(status: "open").count < 10
+      @user.create_appointment(current_user, @user)
+      redirect_to root_path
+    elsif @user.appointments.where(status: "open").count >= 10
+      redirect_to root_path, notice: "Doctor is busy. Shedule consultation later!"
+    end
   end
 
   def update
     @appointment = Appointment.find(params[:id].to_i).update(recomendation: params[:recomendation], status: "closed")
     redirect_to root_path
-  end
-
-  def user_params
-    params.require(:appointment).permit(:id)
   end
 end
