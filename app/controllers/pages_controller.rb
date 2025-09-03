fe5d2bab908a37715ade505fb11190b506965697
Dima <kuzmichman92@gmail.com>
2025-09-03 03:11:23 +0300

    changed_filter_of_categories

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,5 @@
 class PagesController < ApplicationController
   def index
-    @doctors = User.where(role: "doctor")
-  end
-
-  def filter_categories
     @doctors = User.where(role: "doctor").joins(:categories).where("categories.name" => params[:search_category])
   end
 end
