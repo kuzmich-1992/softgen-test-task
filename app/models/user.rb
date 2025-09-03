49d4aeaef9984b43c860165e0b67e50d66611488
Dima <kuzmichman92@gmail.com>
2025-09-03 14:07:44 +0300

    authetication_phone_or_email_added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  attr_accessor :email_confirmation, :login
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
@@ -16,6 +18,8 @@ class User < ApplicationRecord
   has_many :creators, through: :appointments, source: :creator
 
   has_and_belongs_to_many :categories
+
+  validates_format_of :phone, with: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/, :multiline => true, :allow_blank => true
   
   enum role: {admin: 0, doctor: 1, patient: 2}
 
@@ -40,4 +44,18 @@ class User < ApplicationRecord
   def remove_doctor_from_ctegory(doctor_id, category_name)
     User.find(doctor_id).categories.clear
   end
+
+  def login
+    @login || self.phone || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_hash).where(["lower(phone) ILIKE '%#{login.downcase}%' OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:phone) || conditions.has_key?(:email)
+      where(conditions.to_hash).first
+    end
+  end
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_09_02_184555) do
+ActiveRecord::Schema[7.0].define(version: 2025_09_03_103010) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,6 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_09_02_184555) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role", default: 2
+    t.string "phone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
